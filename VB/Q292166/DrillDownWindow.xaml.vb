d8d2047e0d3dee1046af9d3ab37f0f3d95cbdeef
DevExpressExampleBot <fakeEmail>
2022-05-24 09:28:15 +0400

    Source auto update [skip ci]

--- VB/Q292166/DrillDownWindow.xaml.vb
+++ VB/Q292166/DrillDownWindow.xaml.vb
@@ -1,48 +1,53 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
 Imports System.Windows
 Imports DevExpress.Xpf.PivotGrid
 
 Namespace Q292166
-	''' <summary>
-	''' Interaction logic for DrillDownWindow.xaml
-	''' </summary>
-	Partial Public Class DrillDownWindow
-		Inherits Window
-		Implements IDrillDownUpdateableWindow
-		Private fPivot As PivotGridControl
-		Private fColumnValues() As Object
-		Private fRowValues() As Object
 
-		Private Sub New()
-			InitializeComponent()
-		End Sub
+    ''' <summary>
+    ''' Interaction logic for DrillDownWindow.xaml
+    ''' </summary>
+    Public Partial Class DrillDownWindow
+        Inherits Window
+        Implements IDrillDownUpdateableWindow
 
-		Public Sub New(ByVal pivot As PivotGridControl, ByVal columnValues() As Object, ByVal rowValues() As Object)
-			Me.New()
-				fPivot = pivot
-				fColumnValues = columnValues
-				fRowValues = rowValues
-				DoRefreshData()
-		End Sub
+        Private fPivot As PivotGridControl
 
-		Private Sub RefreshData() Implements IDrillDownUpdateableWindow.RefreshData
-			DoRefreshData()
-		End Sub
+        Private fColumnValues As Object()
 
-		Protected Overridable Sub DoRefreshData()
-			Dim columnIndex As Integer = fPivot.GetColumnIndex(fColumnValues)
-			Dim rowIndex As Integer = fPivot.GetRowIndex(fRowValues)
-			If columnIndex < 0 OrElse rowIndex < 0 Then
-				drillDownView.DataSource = Nothing
-			Else
-				drillDownView.DataSource = fPivot.CreateDrillDownDataSource(columnIndex, rowIndex)
-			End If
-			drillDownView.PopulateColumns()
-		End Sub
-	End Class
+        Private fRowValues As Object()
 
-	Public Interface IDrillDownUpdateableWindow
-		Sub RefreshData()
-	End Interface
+        Private Sub New()
+            Me.InitializeComponent()
+        End Sub
+
+        Public Sub New(ByVal pivot As PivotGridControl, ByVal columnValues As Object(), ByVal rowValues As Object())
+            Me.New()
+            fPivot = pivot
+            fColumnValues = columnValues
+            fRowValues = rowValues
+            DoRefreshData()
+        End Sub
+
+        Private Sub RefreshData() Implements IDrillDownUpdateableWindow.RefreshData
+            DoRefreshData()
+        End Sub
+
+        Protected Overridable Sub DoRefreshData()
+            Dim columnIndex As Integer = fPivot.GetColumnIndex(fColumnValues)
+            Dim rowIndex As Integer = fPivot.GetRowIndex(fRowValues)
+            If columnIndex < 0 OrElse rowIndex < 0 Then
+                Me.drillDownView.DataSource = Nothing
+            Else
+                Me.drillDownView.DataSource = fPivot.CreateDrillDownDataSource(columnIndex, rowIndex)
+            End If
+
+            Me.drillDownView.PopulateColumns()
+        End Sub
+    End Class
+
+    Public Interface IDrillDownUpdateableWindow
+
+        Sub RefreshData()
+
+    End Interface
 End Namespace
--- VB/Q292166/MainWindow.xaml.vb
+++ VB/Q292166/MainWindow.xaml.vb
@@ -1,5 +1,3 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
 Imports System.Data
 Imports System.Windows
 Imports System.Data.OleDb
@@ -7,50 +5,49 @@ Imports System.Collections
 Imports DevExpress.Xpf.PivotGrid
 
 Namespace Q292166
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
-		Private filtered As Boolean
 
-		Public Sub New()
-			InitializeComponent()
-			salesPersonPivot.DataSource = GetDataSource(False)
-		End Sub
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private Sub OnSalesPersonPivotCellClick(ByVal sender As Object, ByVal e As PivotCellEventArgs)
-			Dim pivot As PivotGridControl = CType(sender, PivotGridControl)
-			CType(New DrillDownWindow(pivot, GetValues(pivot, e.GetColumnFields(), e.ColumnIndex), GetValues(pivot, e.GetRowFields(), e.RowIndex)), DrillDownWindow).Show()
-		End Sub
+        Private filtered As Boolean
 
-		Private Function GetValues(ByVal pivot As PivotGridControl, ByVal fields() As PivotGridField, ByVal lastLevelIndex As Integer) As Object()
-			Dim result As New ArrayList()
-			For Each field As PivotGridField In fields
-				result.Add(pivot.GetFieldValue(field, lastLevelIndex))
-			Next field
-			Return result.ToArray()
-		End Function
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.salesPersonPivot.DataSource = GetDataSource(False)
+        End Sub
 
-		Private Function GetDataSource(ByVal filtered As Boolean) As DataTable
-			Dim [select] As String = "select [Sales Person], CategoryName, [Extended Price] from SalesPerson"
-			If filtered Then
-				[select] = String.Concat([select], " where [Extended Price] > 100")
-			End If
-			Dim adapter As New OleDbDataAdapter([select], "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=..\..\nwind.mdb")
-			Dim source As New DataTable()
-			adapter.Fill(source)
-			Return source
-		End Function
+        Private Sub OnSalesPersonPivotCellClick(ByVal sender As Object, ByVal e As PivotCellEventArgs)
+            Dim pivot As PivotGridControl = CType(sender, PivotGridControl)
+            Call New DrillDownWindow(pivot, GetValues(pivot, e.GetColumnFields(), e.ColumnIndex), GetValues(pivot, e.GetRowFields(), e.RowIndex)).Show()
+        End Sub
 
-		Private Sub OnReloadButtonClick(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			filtered = Not filtered
-			salesPersonPivot.DataSource = GetDataSource(filtered)
-			For Each window As Window In Application.Current.Windows
-				If TypeOf window Is IDrillDownUpdateableWindow Then
-					CType(window, IDrillDownUpdateableWindow).RefreshData()
-				End If
-			Next window
-		End Sub
-	End Class
+        Private Function GetValues(ByVal pivot As PivotGridControl, ByVal fields As PivotGridField(), ByVal lastLevelIndex As Integer) As Object()
+            Dim result As ArrayList = New ArrayList()
+            For Each field As PivotGridField In fields
+                result.Add(pivot.GetFieldValue(field, lastLevelIndex))
+            Next
+
+            Return result.ToArray()
+        End Function
+
+        Private Function GetDataSource(ByVal filtered As Boolean) As DataTable
+            Dim [select] As String = "select [Sales Person], CategoryName, [Extended Price] from SalesPerson"
+            If filtered Then [select] = String.Concat([select], " where [Extended Price] > 100")
+            Dim adapter As OleDbDataAdapter = New OleDbDataAdapter([select], "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=..\..\nwind.mdb")
+            Dim source As DataTable = New DataTable()
+            adapter.Fill(source)
+            Return source
+        End Function
+
+        Private Sub OnReloadButtonClick(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            filtered = Not filtered
+            Me.salesPersonPivot.DataSource = GetDataSource(filtered)
+            For Each window As Window In Application.Current.Windows
+                If TypeOf window Is IDrillDownUpdateableWindow Then CType(window, IDrillDownUpdateableWindow).RefreshData()
+            Next
+        End Sub
+    End Class
 End Namespace
